1cb67b9846051f0f8cc44ba013d089016c706d62
multi05369 <tentingsanook@gmail.com>
2025-08-19 19:02:18 +0700

    feat: Add scheduler not finish

--- web/app.py
+++ web/app.py
@@ -1,43 +1,41 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask import redirect
-from flask import url_for
+import os
+
+from flask import Flask, request, render_template, redirect
 from pymongo import MongoClient
 from bson import ObjectId
 
-sample = Flask(__name__)
+app = Flask(__name__)
 
-client = MongoClient("mongodb://mongo:27017/")
-mydb = client["ipa2025"]
-mycol = mydb["routers"]
+mongo_uri  = os.environ.get("MONGO_URI")
+db_name    = os.environ.get("DB_NAME")
 
+client = MongoClient(mongo_uri)
+db = client[db_name]
+routers = db["routers"]
 
-@sample.route("/")
-def main():
-    routers = mycol.find()
-    return render_template("index.html", data=routers)
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html", routers=list(routers.find()))
 
-@sample.route("/add", methods=["POST"])
-def add_comment():
+@app.route("/add", methods=["POST"])
+def add_router():
     ip_address = request.form.get("ip_address")
     username = request.form.get("username")
     password = request.form.get("password")
 
     if ip_address and username and password:
-        data = { "ip_address": ip_address, "username": username, "password": password }
-        mycol.insert_one(data)
-    return redirect(url_for("main"))
+        routers.insert_one({
+            "ip_address": ip_address,
+            "username": username,
+            "password": password
+        })
+    return redirect("/")
 
-@sample.route("/delete", methods=["POST"])
-def delete_comment():
-    try:
-        router_id = request.form.get("_id")
-        if router_id:
-            mycol.delete_one({ '_id': ObjectId(router_id)})
-    except Exception:
-        pass
-    return redirect(url_for("main"))
+@app.route("/delete/<id>", methods=["POST"])
+def delete_router(id):
+    #id = request.form.get("_id")
+    routers.delete_one({"_id": ObjectId(id)})
+    return redirect("/")
 
 if __name__ == "__main__":
-    sample.run(host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
