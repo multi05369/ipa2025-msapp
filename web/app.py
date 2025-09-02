e01ec6adb57976eb27fb7842adfa1cf3972d7f57
multi05369 <tentingsanook@gmail.com>
2025-09-02 12:34:30 +0700

    refac: fix web lint check

--- web/app.py
+++ web/app.py
@@ -6,16 +6,18 @@ from bson import ObjectId
 
 app = Flask(__name__)
 
-mongo_uri  = os.environ.get("MONGO_URI")
-db_name    = os.environ.get("DB_NAME")
+mongo_uri = os.environ.get("MONGO_URI")
+db_name = os.environ.get("DB_NAME")
 
 client = MongoClient(mongo_uri)
 db = client[db_name]
 routers = db["routers"]
 
+
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html", routers=list(routers.find()))
+
 
 @app.route("/add", methods=["POST"])
 def add_router():
@@ -31,16 +33,23 @@ def add_router():
         })
     return redirect("/")
 
+
 @app.route("/router/<ip_address>", methods=["GET"])
 def router_detail(ip_address):
-    interface_status = db["interface_status"].find({"ip_address": ip_address}).sort("time", -1).limit(3)
-    return render_template("router_detail.html", ip_address=ip_address, interface_status=list(interface_status))
+    interface_status = db["interface_status"].find(
+        {"ip_address": ip_address}
+        ).sort("time", -1).limit(3)
+    return render_template(
+        "router_detail.html",
+        ip_address=ip_address,
+        interface_status=list(interface_status))
+
 
 @app.route("/delete/<id>", methods=["POST"])
 def delete_router(id):
-    #id = request.form.get("_id")
     routers.delete_one({"_id": ObjectId(id)})
     return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
